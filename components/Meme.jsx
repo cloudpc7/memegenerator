a57c15c61d57db2e4741ba17a45c92aeaa4bd15c
cloudpc7 <51870878+cloudpc7@users.noreply.github.com>
2024-05-27 09:52:19 -0700

    adding all files

---
+++ components/Meme.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import memesData from './memesData'
+export default function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    });
+
+    const [allMemeImages] = React.useState(memesData)
+
+    const getMeme = () =>  {
+      const memesArray = allMemeImages.data.memes;
+      const randomNumber = Math.floor(Math.random() * memesArray.length);
+      const url = memesArray[randomNumber].url;
+      setMeme(prevMeme => ({
+        ...prevMeme,
+        randomImage: url
+      }))
+    }
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
+    return (
+        <>
+            <div className="form">
+                <div className="top-text">
+                    <label>Top text</label>
+                    <input 
+                        type="text"
+                        placeholder="Top text"
+                        name="topText"
+                        value={meme.topText}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="bottom-text">
+                    <label>Bottom text</label>
+                    <input 
+                        type="text"
+                        placeholder="Bottom text"
+                        className="form--input"
+                        name="bottomText"
+                        value={meme.bottomText}
+                        onChange={handleChange}  
+                    /> 
+                </div>
+                <button type="submit" className="form--button" onClick={getMeme}>Get a new Meme image</button>
+            </div>
+            <div className="meme">
+                <img src= {meme.randomImage} className="meme--image"/>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
+        </>
+    )
+}
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header'
+import Meme from '../components/Meme'
+import './style.css'
+
+export default function App() {
+  return (
+      <main>
+        <Header />
+        <Meme />
+      </main>
+
+  )
+}
